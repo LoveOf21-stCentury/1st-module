e88e3ee126b551000da61fcfa548be041dadaa3d
Lyubov <l.musilmanbekova12@gmail.com>
2024-11-12 21:55:04 +0300

    added tasks

--- src/secondModule/Main.java
+++ src/secondModule/Main.java
@@ -1,7 +1,30 @@
 package secondModule;
 
 public class Main {
-    public static void main(String[] args) {
+    public static class Cat {
+        public void sayHello(){
+            System.out.println("Мяу!");
+        }
+    }
 
+    public static class Dog{
+        public void sayHello(){
+            System.out.println("Гав!");
+        }
+        public void catchCat(Cat cat){
+            System.out.println("Кошка поймана");
+            sayHello();
+        }
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        cat.sayHello();
+        dog.sayHello();
+        dog.catchCat(cat);
     }
 }
+
+
+
