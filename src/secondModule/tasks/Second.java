302bebfb99af2d73ec1611ccfb2fbc1072b16cb9
Lyubov <l.musilmanbekova12@gmail.com>
2024-11-23 20:48:01 +0300

    added second task

--- src/secondModule/tasks/Second.java
+++ src/secondModule/tasks/Second.java
@@ -1,2 +1,24 @@
-package secondModule.tasks;public class Second {
+package secondModule.tasks;
+
+public class Second {
+    public enum Day {
+        MONDAY("Понедельник"),
+        TUESDAY("Вторник"),
+        WEDNESDAY("Среда"),
+        THURSDAY("Четверг"),
+        FRIDAY("Пятница"),
+        SATURDAY("Суббота"),
+        SUNDAY("Воскресенье");
+        private final String rusNames;
+        Day(String rusNames) {
+            this.rusNames = rusNames;
+        }
+        public boolean isWeekend() {
+            return this == SATURDAY || this == SUNDAY;
+        }
+
+        public String getRusName() {
+            return this.rusNames;
+        }
+    }
 }
